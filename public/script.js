4bdd9ed7d2436b8115fde023d172652edcbf5e6e
shubhamdixit760 <shubhamdixit503@gmail.com>
2021-10-16 20:35:39 +0530

    fixed server

--- public/script.js
+++ public/script.js
@@ -2,7 +2,7 @@ const socket = io('/')
 const videoGrid = document.getElementById('video-grid')
 const myPeer = new Peer(undefined,{
     host:'/',
-    port:process.env.PORT+1
+    port:3001
 })
 
 const myVideo = document.createElement('video')
